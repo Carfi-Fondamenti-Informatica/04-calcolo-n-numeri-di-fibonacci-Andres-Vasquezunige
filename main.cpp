8dbbf1bfd8feed6b9a6496ce3ac4bebefa031e57
Andres-Vasquezunige <116440246+Andres-Vasquezunige@users.noreply.github.com>
2022-11-23 16:55:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
+int main(){
+    int n=0,p=1;int s=1;
+cin>>n;
+if(n>=2){
+    cout<<p<<endl<<s<<endl;
+    int i=3;
+    while(i++,i<=n){
+        cout<<p+s<<endl;
+        if(p<s){
+            p+=s;
+        }
+        else {
+            s+=p;
+        }
+    }
+}
+else{
+    cout<<"errore";
+}
 
-int main() {
-   return 0;
+    return 0;
 }
