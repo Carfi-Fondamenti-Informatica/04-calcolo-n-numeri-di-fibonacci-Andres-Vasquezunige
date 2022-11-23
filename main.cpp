f0d917e524a0fd41b30abcd0f3a50b1b6b8edac0
Andres-Vasquezunige <116440246+Andres-Vasquezunige@users.noreply.github.com>
2022-11-23 17:17:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n=0,p=1;int s=1;
+    int n=0;
+    int p=1;
+    int s=1;
 cin>>n;
 if(n>=2){
     cout<<p<<endl<<s<<endl;
     int i=3;
-    while(i++,i<=n){
+    while(i++<=n){
         cout<<p+s<<endl;
         if(p<s){
             p+=s;
